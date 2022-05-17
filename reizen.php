c25d8784f989d6229362428de927c4950258a7cb
David <74043362+84228@users.noreply.github.com>
2022-05-17 22:41:07 +0200

    Add files via upload

--- reizen.php
+++ reizen.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <?php 
 	session_start();
-
     // To only give access to a page if a user is logged in  
 
     // if(!isset($_SESSION['username'])){
@@ -35,8 +34,8 @@
 
     }
     $ulvl = $_SESSION['user-lvl'];
-    $reisID   = $_SESSION['reisID'];
-    // $ID = $_GET['inschrijving'];
+    $userID   = $_SESSION['userID'];
+    $ID = $_GET['count'];
     // verifieer lvl van persoon (admin lvl req)
     if ( $ulvl == "2") {
     //site here
@@ -56,30 +55,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="scss/main.css">
-    <title>Reizen Inschrijvingen</title>
+    <title>Examen Training</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Ga Lekker Reizen</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="overzicht.php">Overzicht</a>
-                    </li>
-                    <li class="nav-item">
-                        <?php echo $login; ?>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+        <?php
+            include_once "nav.php";
+        ?>
     <?php require_once 'reizen_process.php'; ?>
 
     <?php 
@@ -92,97 +73,110 @@
                 unset ($_SESSION['message']);
             ?>
 
-        </div>
+        </div> 
 
     <?php endif ?>
-    <h1> Inschrijven voor: </h1>
+    <h1> Toevoegen Gebruiker </h1>
         <div class="container">
         <?php 
             //call naar de config file
             require_once 'config.php';
-           
-            //laat alles zien van de databse tafel: "inschrijvingen"
-            // $ID = isset($_GET['inschrijving']) ?$_GET['inschrijving'] :"";
-            $result = $mysqli->query("SELECT $reisID FROM reizen WHERE reisID=$reisID") or die($mysqli->error);
-            echo $ID;
-            // pre_r($result);
-            // pre_r($result->fetch_assoc());
+            //laat alles zien van de databse tafels: "reizen en inschrijvingen"
+            $result = $mysqli->query("SELECT * FROM reizen WHERE reisID=$ID") or die($mysqli->error());
+            //laat alle gebruikers die ingeschreven zijn zien 
+            $result2 = $mysqli->query("SELECT * FROM Users WHERE reisID=$ID") or die($mysqli->error());
+
+            // test of de content wel laat
+            //  pre_r($result2);
+            //  pre_r($result2->fetch_assoc());
             ?>
 
             <div class="row justify-content-center">
                 <table class="table"> 
                     <thead>
                         <tr>
-                            <th>Naam</th>
-                            <th>Studentennummer</th>
-                            <th>ReisID</th>
-                            <th>Opmerkingen</th>
-                            <th colspan="2">Action</th> 
+                            <th>Titel</th>
+                            <th>Bestemming</th>
+                            <th>Omschrijving</th>
+                            <!-- om te checken of het de juiste reis is  -->
+                            <th>reisID</th>
                         </tr>
                     </thead>
-                    <?php
-                        while ($row = $result->fetch_assoc()): ?>
-                            <tr>
-                                <td><?= $row['naam']; ?></td> 
-                                <td><?= $row['studentennummer']; ?></td> 
-                                <td><?= $row['reisID']; ?></td>
-                                <td><?= $row['opmerkingen']; ?></td>  
-                                <td>
-                                <!-- $reisID == $row['reisID']; ||  -->
-                                    <?php if( $ulvl == "2" ) { ?>
-                                        <a href="reizen.php?edit=<?= $row['id']; ?>" class="btn btn-info">Edit</a>
+                <?php
+                    while ($row = $result->fetch_assoc()): ?>
+                        <tr>
+                            <td><?= $row['titel']; ?></td> 
+                            <td><?= $row['bestemming']; ?></td> 
+                            <td><?= $row['omschrijving']; ?></td> 
+                            <td><?= $row['reisID']; ?></td> 
+                        </tr>
 
-                                        <?php } else { } ?>
-
-                                    <?php 
-                                        // verifieer lvl van persoon (admin lvl req)
-                                        if ( $ulvl == "2") {
-                                            //site here
-                                            ?>
-                                            <a href="reizen_process.php?delete=<?= $row['id']; ?>" class="btn btn-danger">Delete</a>
-                                            <?php } 
-                                        //level 1 
-                                            else{
-                                                
-                                            } 
-                                    ?>
-                                </td>
-                            </tr>
                         <?php endwhile; ?>
+
+                </table>
+                <table class="table"> 
+                    <thead>
+                        <tr>
+                            <th>Studentennummer</th>
+                            <th>Naam</th>
+                            <th>Opmerkingen</th>
+                            <!-- om te checken of het de juiste reis is  -->
+                            <th>reisID</th>
+                        </tr>
+                    </thead>
+                <?php
+                    while ($row2 = $result2->fetch_assoc()): ?>
+                        <tr>
+                            <td><?= $row2['studentennummer']; ?></td> 
+                            <td><?= $row2['naam']; ?></td> 
+                            <td><?= $row2['opmerkingen']; ?></td> 
+                            <td><?= $row2['reisID']; ?></td> 
+                            <!-- student kan zichzelf alleen verwijderen en moet alleen opmerkingen kunnen veranderen -->
+                            <td>
+                                <?php if( $userID == $row2['id'] || $ulvl == "2" ) { ?>
+                                    <a href="reizen.php?edit=<?= $row2['id']; ?>" class="btn btn-info">Edit</a>
+
+                                    <?php } else { } ?>
+
+                                <?php 
+                                    // verifieer lvl van persoon (admin lvl req)
+                                    if ( $userID == $row2['id'] || $ulvl == "2") {
+                                        //site here
+                                          ?>
+                                          <a href="reizen_process.php?delete=<?= $row2['id']; ?>" class="btn btn-danger">Uitschrijven</a>
+                                        <?php } 
+                                       //level 1 
+                                        else{
+                                            
+                                        } 
+                                ?>
+                            </td>
+                        </tr>
+                    <?php endwhile; ?>
                 </table>
             </div>
 
             <?php 
             //functie voor het uitlezen van de database table
             //gebruikt bij het testen
-            function pre_r( $array ) {
-                echo '<pre>';
-                print_r($array);
-                echo '</pre>';
-            }
+            // function pre_r( $array ) {
+            //     echo '<pre>';
+            //     print_r($array);
+            //     echo '</pre>';
+            // }
 
         ?>
+        
         <!-- de form -->
         <div class="row justify-content-center">
             <form action="reizen_process.php" method="POST"> 
-            <?php 
-                 $id   = $_SESSION['id'];
-                 $naam   = $_SESSION['naam'];
-                 $student   = $_SESSION['studentennummer'];
-                 $result2 = $mysqli->query("SELECT `counter` FROM reizen WHERE $ID") or die($mysqli->error);
-                 $row2 = $result2->fetch_assoc();
-                 $counter = $row2['counter']; + 1; 
-                //  $counter2 = $row2['counter']; - 1; 
-                 ?>
-
-                <input type="hidden" name="id" value="<?= $id; ?>">
                 <input type="hidden" name="ID" value="<?= $ID; ?>">
-                <input type="hidden" name="counter" value="<?= $counter; ?>">
-                <input type="hidden" name="naam" value="<?= $naam; ?>">
                 <input type="hidden" name="studentennummer" value="<?= $student; ?>">
+                <input type="hidden" name="naam" value="<?= $name; ?>">
+                <input type="hidden" name="counter" value="<?= $counter; ?>">
                 <div class="form-group"> 
-                    <label>opmerkingen</label>
-                    <input type="text" name="opmerkingen" class="form-control" value="" placeholder="Enter your opmerkingen"> 
+                    <label>Opmerkingen</label>
+                    <input type="text" name="opmerkingen" class="form-control" value="<?= $opmerkingen; ?>" placeholder="Vul hier je opmerkingen in" required> 
                 </div> 
                 <div class="form-group button">
                     <!-- verander de knop van Save naar Update  -->
@@ -190,7 +184,7 @@
                         if ($update == true) { 
                     ?>
                         <button type="submit" class="btn btn-info" name="update">Update</button> 
-                        <a href="index.php" class="btn btn-secondary">Back</a>
+                        <a href="reizen.php" class="btn btn-secondary">Back</a>
                     <?php } else { ?> 
                             <button type="submit" class="btn btn-primary" name="save">Save</button> 
                     <?php } ?>
@@ -203,5 +197,3 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
 </body>
 </html>
-
-
